4281ef59009c67cc219fddb53704cbd774649bda
DuongTanTen <tienduongtan@gmail.com>
2026-08-18 11:56:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ with tab1:
                 }
                 
                 response = client.models.generate_content(
-                    model='gemini-2.0-flash',
+                    model='gemini-1.5-flash',
                     contents=prompt,
                     config=types.GenerateContentConfig(
                         response_mime_type="application/json",
